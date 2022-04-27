40bec803d0f07c6d787f9a25f473abe45c58afc4
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2022-04-27 11:23:52 -0400

    Update numbergame.py
    Added a score system

--- numbergame.py
+++ numbergame.py
@@ -2,11 +2,13 @@ import random
 import time
 
 number = random.randint(1, 100)
+score = float(1)
 
-print("The goal of the game is to guess the number!")
+print("The goal of the game is to guess the number, and get the lowest score possible!")
 time.sleep(1)
 print("You will be told if the number you guessed is greater or lesser than the actual number!")
 time.sleep(1)
+print("Pick a number between 1 and 100!")
 
 while True:
     guess = float(input("Enter your guess!"))
@@ -15,10 +17,13 @@ while True:
         time.sleep(1)
         print("You win!")
         time.sleep(1)
+        print("Final score: ", score)
         break
     if guess > number:
         print("The number is lower than", guess)
+        score = score + 1
         time.sleep(1)
     if guess < number:
         print("The number is greater than", guess)
+        score = score + 1
         time.sleep(1)
