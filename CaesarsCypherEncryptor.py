a7b34a437a1407836d5679163c2f064632d0e8bc
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2022-05-12 11:18:31 -0400

    Update CaesarsCypherEncryptor.py
    Bug fixes

--- CaesarsCypherEncryptor.py
+++ CaesarsCypherEncryptor.py
@@ -1,19 +1,22 @@
+import time 
+
 def encypt_func(txt, s):  
     result = ""  
-  
     for i in range(len(txt)):  
         char = txt[i]   
-  
         if (char.isupper()):  
             result += chr((ord(char) + s - 64) % 26 + 65)  
         else:  
             result += chr((ord(char) + s - 96) % 26 + 97)  
-    return result 
-
-#Enter here
-txt = "Created by Random Demon"  
+    return result
+    
+txt = input("What would you like to encrypt?")  
 s = 4
-  
+
+time.sleep(1)
 print("Plain txt : " + txt)  
+time.sleep(1)
 print("Shift pattern : " + str(s))  
+time.sleep(1)
 print("Cipher: " + encypt_func(txt, s))  
+time.sleep(3)
