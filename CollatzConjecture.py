5694ab44321eb0e581a190d2002ca87e5fa91733
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2022-05-16 09:06:41 -0400

    Add files via upload
    Some pretty cool projects

---
+++ CollatzConjecture.py
@@ -0,0 +1,15 @@
+val = 2
+tick = 1
+num = 2
+
+while True:
+    if (val%2)==0:
+        val/=2
+    elif val==1:
+        print(tick, " works!")
+        tick+=1
+        num+=1
+        val==tick
+    elif (val%2)!=0:
+        val*=3
+        val+=1
---
+++ infiPrinter.py
@@ -0,0 +1,4 @@
+n = 0
+while True:
+    n+=1 
+    print(n)
