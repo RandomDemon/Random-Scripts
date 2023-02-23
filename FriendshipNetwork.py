eb54e3ab7d7ead1c2f375d95e437acf820e8073d
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-02-23 10:49:41 -0500

    Update FriendshipNetwork.py

--- FriendshipNetwork.py
+++ FriendshipNetwork.py
@@ -9,19 +9,32 @@ for a, b in ab:
     friend[a-1].add(b-1)
     friend[b-1].add(a-1)
 
+ans = []from collections import defaultdict
+
+input_ = input
+n, m = map(int, input_().split())
+ab = [list(map(int, input_().split())) for _ in range(m)]
+
+friend = defaultdict(set)
+for a, b in ab:
+    friend[a-1].add(b-1)
+    friend[b-1].add(a-1)
+
 ans = []
-for query in queries:
+friends_of_1 = friend[0]
+for query in range(m):
+    query = list(map(int, input_().split()))
     if query[0] == 1:
         friend[query[1]-1].add(query[2]-1)
         friend[query[2]-1].add(query[1]-1)
     elif query[0] == 2:
-        friend[queery[1]-1].discard(query[2]-1)
-        friend[queery[2]-1].discard(query[1]-1)
+        friend[query[1]-1].discard(query[2]-1)
+        friend[query[2]-1].discard(query[1]-1)
     else:
-        res = set()
-        for i in range(n):
-            res.add(i)
-            for j in friend[1]:
-                res.discard(j)
+        res = set(range(n))
+        for f in friends_of_1:
+            res.discard(f)
+            res -= friend[f]
         ans.append(len(res))
+
 print(*ans, sep='\n')
